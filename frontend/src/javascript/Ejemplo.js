436884820947da6926a6d8059292289c79549258
BryanZavala69420 <Bryan.tzm332@gmail.com>
2025-11-05 15:47:24 -0700

    CSS mejorado

--- frontend/src/javascript/Ejemplo.js
+++ frontend/src/javascript/Ejemplo.js
@@ -1,48 +1,40 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom"; // Asegúrate de usar 'react-router-dom', no 'react-router'
+import { Link, useNavigate } from "react-router-dom";
 import faggot from '../imagenes/faggot.gif';
+import '../css/Ejemplo.css';
 
 function Ejemplo() {
     const sesionActiva = localStorage.getItem("sesionIniciada") === "true";
     const usuarioNombre = localStorage.getItem("usuarioNombre");
     const usuarioRol = localStorage.getItem("usuarioRol");
-    const Navegar = useNavigate();//luego lo arreglo XD
-
+    const Navegar = useNavigate(); // luego lo arreglo XD
 
     return (
-        <div>
+        <div className="ejemplo-container">
             <div className="hola">
                 <h1>Hola mundo</h1>
             </div>
-            {sesionActiva ? ( //esto ha de cuentas que es un if, si la sesion esta activa va y checa el rol del usuario
-                usuarioRol === "2" ? (  // Si el rol es del admin, en la base de datos deberia ser 1, o 2, la neta luego lo cambio
-                    <div>
+
+            {sesionActiva ? (
+                usuarioRol === "2" ? (
+                    <div className="mensaje admin">
                         <p>Bienvenido, {usuarioNombre}! Eres un Administrador.</p>
                         <img src={faggot} alt="Fag" width="600" />
                     </div>
-                ) : ( //ya checo si la sesion esta activa, pero como no encontro que el rol es 1, o 2, la neta no me acuerdo, entonces pasa al usuario normal , este es un if anidado ha de cuentas
-                    <div>
+                ) : (
+                    <div className="mensaje user">
                         <p>Bienvenido, {usuarioNombre}! Eres un usuario normal.</p>
                     </div>
                 )
-
-            ) : ( //else, si la sesion no esta activa, entonces nos manda para que la iniciemos, jajkasjdksdkj
-                <div>
+            ) : (
+                <div className="mensaje">
                     <p>Inicia sesión para acceder a esta página.</p>
                     <Link to="/sesion">Iniciar sesión</Link>
-
-
                 </div>
             )}
 
-
-
-
             <Link to="/">Regresar</Link>
-
-
         </div>
-
     );
 }
 
