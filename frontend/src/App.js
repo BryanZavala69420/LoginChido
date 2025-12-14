8f65b1ec4abb333e97416dcda736416706448b40
BryanZavala69420 <Bryan.tzm332@gmail.com>
2025-12-13 23:12:19 -0700

    AAAAA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,7 +33,7 @@ function App() {
         <Route path='/nosotros' element={<SobreNosotros />} />
 
         <Route path="*" element={<Error404 />} />
-      </Routes>
+      </Routes> 
     </BrowserRouter>
   );
 }
